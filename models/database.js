6e4fa7648eba68359b25b10439b20795597c0431
Jonathan <j_moreno@uncg.edu>
2024-09-16 14:10:08 -0400

    npm modules

--- models/database.js
+++ models/database.js
@@ -5,7 +5,7 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "", // insert your own password
   database: "nodemysql",
 });
 
